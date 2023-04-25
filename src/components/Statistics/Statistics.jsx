2c9387761f3bcc3dfbe83fc2372a8ff62901bdd1
Oleksii Yasynetskyi <ks7495395@gmail.com>
2023-04-25 19:21:47 +0300

    fix

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -7,27 +7,46 @@ function getRandomColor() {
 }
 
 
-export default function Statistics({ items }) {
+// export default function Statistics({ items }) {
 
-  return <div className={st.statistics}>
-    <h2 className={st.title}>Upload stats</h2>
+//   return <div className={st.statistics}>
+//     <h2 className={st.title}>Upload stats</h2>
 
-    <ul className={st.stats}>
-      {items.map(({ id, label, percentage }) => (
+//     <ul className={st.stats}>
+//       {items.map(({ id, label, percentage }) => (
         
-        <li className={st.item} key={id} style={{backgroundColor: getRandomColor()}}>
-              <span className={st.label}>{label}</span>
-              <span className={st.percentage}>{percentage}%</span>
-        </li>
-      ))}
+//         <li className={st.item} key={id} style={{backgroundColor: getRandomColor()}}>
+//               <span className={st.label}>{label}</span>
+//               <span className={st.percentage}>{percentage}%</span>
+//         </li>
+//       ))}
+//       </ul>
+//     </div>
+// }
+
+
+export default function Statistics({ title, items }) {
+  return (
+    <div className={st.statistics}>
+      {title && <h2 className={st.title}>{title}</h2>}
+
+      <ul className={st.stats}>
+        {items.map(({ id, label, percentage }) => (
+          <li className={st.item} key={id} style={{ backgroundColor: getRandomColor() }}>
+            <span className={st.label}>{label}</span>
+            <span className={st.percentage}>{percentage}%</span>
+          </li>
+        ))}
       </ul>
     </div>
+  );
 }
 
 Statistics.propTypes = {
   items: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
+      titel: PropTypes.string.isRequired,
       label: PropTypes.string.isRequired,
       percentage: PropTypes.number.isRequired,
     })
